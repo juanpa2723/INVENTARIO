76d3cb32cb370fa583bddf482e4ad75473f8ce8b
JD Cousc <65689076+JDcousc@users.noreply.github.com>
2021-02-18 17:57:40 -0500

    Create usuario.php

---
+++ proyecto_inventario web (codigos)/informacion/usuario.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+
+      <meta charset="UTF-8">
+      <title>listbalance.com</title>
+      <link rel="stylesheet" href="../css/estilos.css">
+      <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+      <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&display=swap" rel="stylesheet">
+</head>
+<body>
+</nav><br><br><br><br><br>
+<div class= "contenedor head">
+<h1 text align="center" right="20" >usuarios</h1><br><br>
+    <p class= "copy" text align="center"></p>
+
+</div>
+
+	<header class="header">
+		<div class="container">
+		<div class="btn-menu">
+			<label for="btn-menu">☰</label>
+		</div>
+			<div class="logo">
+            <img src="../img/logo.png" width= "200"height= "200">
+			</div>
+			<nav class="menu">
+            <a href="quienes somos.php">Quienes somos</a>
+			<a href="acerca de.php" title="Acerca de" rel="nofollow">Acerca de</a>
+			<a href="contactanos.php" title="Contactanos">Contactanos</a>
+            <a href="terminos.php">Terminos y condiciones</a>
+			</nav>
+		</div>
+	</header>
+	<div class="capa"></div>
+<!--	--------------->
+<input type="checkbox" id="btn-menu">
+<div class="container-menu">
+	<div class="cont-menu">
+		<nav>
+        <a href="../pagina_principal.php"><i class='bx bxs-home'></i> Inicio</a>
+        <a href="producto.php"><i class='bx bxs-shopping-bags'></i> Productos</a>
+        <a href="usuario.php"><i class='bx bxs-user'></i> Usuarios</a>
+        <a href="venta.php"><i class='bx bxs-bar-chart-alt-2'></i> Ventas</a>
+        <a href="reporte.php"><i class='bx bxs-book'></i> Reportes</a>
+        <a href="../index.php"><i class='bx bxs-door-open'></i> Cerrar sesion</a>
+		</nav>
+
+		<label for="btn-menu">✖️</label>
+	</div>
+</div>
+<br><br><br><br><br><br><br><br>
+<body>
+    <p text align="center">
+     Para usar nuestro página web y / o recibir nuestros servicios, debes tener al menos [agregar número] años de edad,<br>
+     o la mayoría de edad legal en tu jurisdicción, y poseer la autoridad legal,el derecho y la libertad para participar<br>
+     en estos Términos como un acuerdo vinculante. No tienes permitido utilizar esta página web y / o recibir servicios<br>
+     si hacerlo está prohibido en tu país o en virtud de cualquier ley o regulación aplicable a tu caso.<br>
+     Términos comerciales ofrecidos a los clientes<br></p>
+    <p text align="center">Esto es otro párrafo.</p>
+   </body>
+
+</body>
+</html>
