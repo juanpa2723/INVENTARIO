8b5847d827fe5c80e3244da170cf1903e5ef0b01
JD Cousc <65689076+JDcousc@users.noreply.github.com>
2021-02-18 17:57:57 -0500

    Create pagina_principal.php

---
+++ proyecto_inventario web (codigos)/pagina_principal.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+
+      <meta charset="UTF-8">
+      <title>listbalance.com</title>
+      <link rel="stylesheet" href="css/estilos.css">
+      <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+      <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&display=swap" rel="stylesheet">
+</head>
+<body> 
+	<header class="header">
+		<div class="container">
+		<div class="btn-menu">
+			<label for="btn-menu">☰</label>    
+		</div>
+		<div class="logo">
+		<img src="img/logo.png" width= "200"height= "200">
+		</div>
+			<nav class="menu">
+			<a href="informacion/quienes somos.php">Quienes somos</a>
+			<a href="informacion/acerca de.php" title="Acerca de" rel="nofollow">Acerca de</a>
+			<a href="informacion/contactanos.php" title="Contactanos">Contactanos</a>
+            <a href="informacion/terminos.php">Terminos y condiciones</a>
+			</nav>
+		</div>
+	</header>
+	<div class="capa"></div>
+<!--	--------------->
+<input type="checkbox" id="btn-menu">
+<div class="container-menu">
+	<div class="cont-menu">
+		<nav>
+        <a href="pagina_principal.php"><i class='bx bxs-home'></i> Inicio</a>
+        <a href="informacion/producto.php"><i class='bx bxs-shopping-bags'></i> Productos</a>
+        <a href="informacion/usuario.php"><i class='bx bxs-user'></i> Usuarios</a>
+        <a href="informacion/venta.php"><i class='bx bxs-bar-chart-alt-2'></i> Ventas</a>
+        <a href="informacion/reporte.php"><i class='bx bxs-book'></i> Reportes</a>
+        <a href="index.php"><i class='bx bxs-door-open'></i> Cerrar sesion</a>
+		</nav>
+
+		<label for="btn-menu">✖️</label>
+	</div>
+</div>
+
+</body>
+</html>
