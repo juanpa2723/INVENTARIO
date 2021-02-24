7413a1abfce227e8243d69de6fbaf8a78aff54af
JD Cousc <65689076+JDcousc@users.noreply.github.com>
2021-02-23 22:28:04 -0500

    Create reporte.php

---
+++ proyecto_inventario web (codigos)/informacion/reporte/reporte.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+
+      <meta charset="UTF-8">
+      <title>listbalance.com</title>
+      <link rel="stylesheet" href="css/estilos.css">
+      <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+      <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&display=swap" rel="stylesheet">
+      <link rel="stylesheet" href="css/font-awesome.min.css">
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+      <link rel="stylesheet" href="css/styles.css">
+
+</head>
+<body>
+</nav><br><br><br><br><br>
+
+	<header class="header">
+		<div class="container">
+		<div class="btn-menu">
+			<label for="btn-menu">☰</label>
+		</div>
+			<div class="logo">
+            <img src="img/logo.png" width= "200"height= "200">
+			</div>
+			<nav class="menu">
+            <a href="../quienes somos.php">Quienes somos</a>
+			<a href="../acerca de.php" title="Acerca de" rel="nofollow">Acerca de</a>
+			<a href="../contactanos.php" title="Contactanos">Contactanos</a>
+            <a href="../terminos.php">Terminos y condiciones</a>
+			</nav>
+		</div>
+	</header>
+	<div class="capa"></div>
+
+<input type="checkbox" id="btn-menu">
+<div class="container-menu">
+	<div class="cont-menu">
+		<nav>
+        <a href="../pagina_principal.php"><i class='bx bxs-home'></i> Inicio</a>
+        <a href="../producto/producto.php"><i class='bx bxs-shopping-bags'></i> Productos</a>
+        <a href="../usuario.php"><i class='bx bxs-user'></i> Usuarios</a>
+        <a href="../venta.php"><i class='bx bxs-bar-chart-alt-2'></i> Ventas</a>
+        <a href="reporte/reporte.php"><i class='bx bxs-book'></i> Reportes</a>
+        <a href="../index.php"><i class='bx bxs-door-open'></i> Cerrar sesion</a>
+		</nav>
+
+		<label for="btn-menu">✖️</label>
+	</div>
+</div>
+
+<body>
+<div class="container">    
+    <div class="row">
+        <div id="content" class="col-lg-12">
+        </div>
+    </div>   
+
+<table width="400px" cellpadding="10px" cellspacing="10px" border="1"style="margin: 0 auto;" color: #FF0000;>
+    <tr bgcolor="#3b83bd">
+        <td>Codigo</td>
+        <td>Descripcion</td>
+        <td>Cantidad</td>
+        <td>Precio*Unidad</td>
+    </tr>
+    <tr >
+        <td>1</td>
+        <td>Arroz diana 1kilo</td>
+        <td>20</td>
+        <td>2300</td>
+    </tr>
+    <tr >
+        <td>2</td>
+        <td>Postobon gaseosa 2litros</td>
+        <td>10</td>
+        <td>2500</td>
+    <tr>
+        <td>3</td>
+        <td>Refisal </td>
+        <td>20</td>
+        <td>1500</td>
+    </tr>
+    <tr>
+        <td>4</td>
+        <td>Azucar la morena</td>
+        <td>20</td>
+        <td>1200</td>
+    </tr>       
+    <tr>
+        <td>5</td>
+        <td>Huevos</td>
+        <td>100</td>
+        <td>300</td>
+    </tr>
+  
+</table>
+<br> 
+
+<div style="text-align:center;">
+<a class="btn btn-primary" href="reporteprofesionales.php"><i class="fa fa-download"></i> Descargar PDF</a>
+    
+</div>
+
+</body>
+
+</body>
+</html>
