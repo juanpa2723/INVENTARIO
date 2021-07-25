88c9dd700fe7edd82585e3d254af652b9612b842
JD Cousc <65689076+JDcousc@users.noreply.github.com>
2021-07-25 10:47:57 -0500

    Create quienes somos.php

---
+++ App_proyecto/proyecto_inventario web (codigos)/informacion/quienes somos.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+
+      <meta charset="UTF-8">
+      <title>listbalance.com</title>
+      <link rel="stylesheet" href="../css/estilos.css">
+      <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+      <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&display=swap" rel="stylesheet">
+</head>
+<body>
+</nav>
+
+	<header class="header">
+		<div class="container">
+		<div class="btn-menu">
+			<label for="btn-menu">☰</label>
+		</div>
+			<div class="logo">
+            <img src="../img/logo.png" width= "200"height= "200">
+			</div>
+			<nav class="menu">
+            <a href="quienes somos.php">Quienes somos</a>
+			<a href="acerca de.php" title="Acerca de" rel="nofollow">Acerca de</a>
+			<a href="contactanos.php" title="Contactanos">Contactanos</a>
+            <a href="terminos.php">Terminos y condiciones</a>
+			</nav>
+		</div>
+	</header>
+	<div class="capa"></div>
+<!--	--------------->
+<input type="checkbox" id="btn-menu">
+<div class="container-menu">
+	<div class="cont-menu">
+		<nav>
+        <a href="pagina_principal.php"><i class='bx bxs-home'></i> Inicio</a>
+        <a href="producto/producto.php"><i class='bx bxs-shopping-bags'></i> Productos</a>
+        <a href="usuario.php"><i class='bx bxs-user'></i> Usuarios</a>
+        <a href="venta.php"><i class='bx bxs-bar-chart-alt-2'></i> Ventas</a>
+        <a href="reporte/reporte.php"><i class='bx bxs-book'></i> Reportes</a>
+        <a href="../index.php"><i class='bx bxs-door-open'></i> Cerrar sesion</a>
+		</nav>
+
+		<label for="btn-menu">✖️</label>
+	</div>
+</div>
+<br><br><br><br><br><br><br><br><br><br><br>
+
+    <p class= "p" text align="center">
+        <div class="texto">
+
+        <h2><p text align="center">Quienes somos</p></h2><br><br><br>
+
+     Somos aprendices del Servicio Nacional de Aprendizaje (SENA).<br>
+     Nuestro grupo esta conformado por 4 personas y contamos con la dedicacion,<br> 
+     responsabilidad y calidad para mejorar cada dia y brindarle al cliente un buen sistema web.<br>
+     Cada uno de nosotros contamos con distintos apectos y cualidades necesarias para<br> 
+     solucionar cualquier pregunta, queja o reclamo y ofrcer la ayuda necesaria para<br> 
+     poder garantizar un buen rendimiento del sistema</p></div>
+
+
+</body>
+</html>
