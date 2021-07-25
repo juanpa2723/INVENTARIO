61cfafc465957763fd2de222d11154ec72af1b5a
JD Cousc <65689076+JDcousc@users.noreply.github.com>
2021-07-25 10:44:21 -0500

    Create datos.php

---
+++ App_proyecto/proyecto_inventario web (codigos)/datos.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+
+      <meta charset="UTF-8">
+      <title>listbalance.com</title>
+      <link rel="stylesheet" href="css/estilos.css">
+      <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&display=swap" rel="stylesheet">
+</head>
+<body>
+</nav>
+
+	<header class="header">
+
+		<div class="logo">
+        <img src="img/logo.png" width= "200"height= "200">
+		</div>
+        </header>
+
+	<div class="capa"></div>
+
+    <p class= "p" text align="center">
+    <div class="texto">
+    <br><br><br>
+    <h2><p text align="center">Quienes somos<a name ="quienes somos"> </a></p></h2><br><br>
+
+     Somos aprendices del Servicio Nacional de Aprendizaje (SENA).<br>
+     Nuestro grupo esta conformado por 4 personas y contamos con la dedicacion,<br> 
+     responsabilidad y calidad para mejorar cada dia y brindarle al cliente un buen sistema web.<br>
+     Cada uno de nosotros contamos con distintos apectos y cualidades necesarias para<br> 
+     solucionar cualquier pregunta, queja o reclamo y ofrcer la ayuda necesaria para<br> 
+     poder garantizar un buen rendimiento del sistema</p></div>
+
+     </div>
+     <br><br><br>
+
+    <p class= "p" text align="center">
+        <div class="texto">
+
+        <h2><p text align="center">Acerca de</p></h2><br><br>
+
+     Somos aprendices del Servicio Nacional de Aprendizaje (SENA).<br>
+     Nuestro grupo esta conformado por 4 personas y contamos con la dedicacion,<br> 
+     responsabilidad y calidad para mejorar cada dia y brindarle al cliente un buen sistema web.<br>
+     Cada uno de nosotros contamos con distintos apectos y cualidades necesarias para<br> 
+     solucionar cualquier pregunta, queja o reclamo y ofrcer la ayuda necesaria para<br> 
+     poder garantizar un buen rendimiento del sistema</p></div>
+
+     </div>
+     <br><br><br>
+
+    <p class= "p" text align="center">
+        <div class="texto">
+
+        <h2><p text align="center">Contactanos</p></h2><br><br>
+
+     Somos aprendices del Servicio Nacional de Aprendizaje (SENA).<br>
+     Nuestro grupo esta conformado por 4 personas y contamos con la dedicacion,<br> 
+     responsabilidad y calidad para mejorar cada dia y brindarle al cliente un buen sistema web.<br>
+     Cada uno de nosotros contamos con distintos apectos y cualidades necesarias para<br> 
+     solucionar cualquier pregunta, queja o reclamo y ofrcer la ayuda necesaria para<br> 
+     poder garantizar un buen rendimiento del sistema</p></div>
+
+     </div>
+     <br><br><br>
+
+    <p class= "p" text align="center">
+        <div class="texto">
+
+        <h2><p text align="center">Terminos y condiciones</p></h2><br><br>
+
+     Somos aprendices del Servicio Nacional de Aprendizaje (SENA).<br>
+     Nuestro grupo esta conformado por 4 personas y contamos con la dedicacion,<br> 
+     responsabilidad y calidad para mejorar cada dia y brindarle al cliente un buen sistema web.<br>
+     Cada uno de nosotros contamos con distintos apectos y cualidades necesarias para<br> 
+     solucionar cualquier pregunta, queja o reclamo y ofrcer la ayuda necesaria para<br> 
+     poder garantizar un buen rendimiento del sistema</p></div>
+    </body>
+    </html>
