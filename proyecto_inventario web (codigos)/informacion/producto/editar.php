7e28f6f9346c11c37a6aa0f4997f2e251e86d180
JD Cousc <65689076+JDcousc@users.noreply.github.com>
2021-07-26 12:33:38 -0500

    Delete editar.php

--- proyecto_inventario web (codigos)/informacion/producto/editar.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $data = file_get_contents("php://input");
-    require "conexion.php";
-    $query = $pdo->prepare("SELECT * FROM productos WHERE id = :id");
-    $query->bindParam(":id", $data);
-    $query->execute();
-    $resultado = $query->fetch(PDO::FETCH_ASSOC);
-    echo json_encode($resultado);
-?>
