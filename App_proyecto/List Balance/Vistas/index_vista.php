1c43e1ffd7f2e0bcd9a4084e4490e0372e018a63
JD Cousc <65689076+JDcousc@users.noreply.github.com>
2021-08-14 12:37:48 -0500

    Create index_vista.php

---
+++ App_proyecto/List Balance/Vistas/index_vista.php
@@ -0,0 +1,216 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+
+  <title>ListBalance.com</title> 
+
+  <meta charset="UTF-8">
+
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" >
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat&family=Open+Sans&display=swap" rel="stylesheet"> 
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/normalize.css@8.0.1/normalize.min.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/glider-js@1.7.3/glider.min.css">
+  <link rel="stylesheet" href="css/estilos_index.css">
+  
+</head>  
+
+<body>
+
+    <header class="header">
+		<div class="container">
+		<div class="btn-menu">   
+		</div>
+
+    <div class="logo">
+	<img src="img/logo3.png" width= "200"height= "200">
+	</div>
+      <nav class="navegacion">
+			<ul class="menu">
+				<li><a href="#">Inicio</a></li>
+				<li><a href="#nosotros">Nosotros</a></li>
+        <li><a href="#acerca">Acerca de</a></li>
+        <li><a href="#contactos">Contactos</a></li>
+				<li><a href="#" class="entrar">Entrar</a>
+					<ul class="submenu">
+						<li><a href="Controladores/Login_controlador.php">Iniciar sesion</a></li>
+						<li><a href="Controladores/Registro_controlador.php">Registrar</a></li>
+					</ul>
+				</li>
+			</ul>
+		</nav>
+        </header>
+
+    <div class="carousel">
+
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+      <div class="item active">
+        <img  class="img" src="img/iventario.jpg" alt="iventario" style="width:1600px; height:550px;">
+        <div class="carousel-caption d-none d-md-block">
+        <h2>LIST BALANCE</h2>
+        <p>Un inventario facil, comodo y sencillo haciendo un click</p>
+      </div>
+      </div>
+
+      <div class="item">
+        <img class="img" src="img/abc.jpg" alt="iventario"  style="width:1600px; height:550px;">
+        <div class="carousel-caption">
+        <h3>TIENDAS Y MICORNEGOCIOS</h3>
+        <p>Al alcance de tu mano</p>
+      </div>
+      </div>
+    
+      <div class="item">
+        <img class="img" src="img/aa.jpg" alt="iventario"  style="width:1600px; height:550px;">
+        <div class="carousel-caption">
+        <h3>SACA PROVECHO A TUS PRODUCTOS</h3>
+        <p>Con este inventario web sera mas comodo y rinde mejor</p>
+      </div>
+      </div>
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</div>
+
+<section class="services contenedor" id="servicio">
+  <h1 class="subtitulo">Nuestro servicio</h1>
+  <div class="contenedor-servicio">
+    <img src="img/nosotros.png" alt="" style="width:300px; height:300px;">
+    <div class="checklist-servicio">
+      <div class="service">
+
+      <h3 id="nosotros" class="n-service">Nosotros</h3>
+      <p class="text1"><br>
+        Hola como estan todos,hola como estan todos,hola como estan todos,hola como estan todos
+        hola como estan todos,hola como estan todos,hola como estan todos,hola como estan todos
+        hola como estan todos,hola como estan todos,hola como estan todos,hola como estan todos
+        Hola como estan todos,hola como estan todos,hola como estan todos,hola como estan todos
+        hola como estan todos,hola como estan todos,hola como estan todos,hola como estan todos
+        hola como estan todos,hola como estan todos,hola como estan todos,hola como estan todos</p>
+</div>
+</div>
+</div>
+
+<div class="contenedor-servicio">
+    
+    <div class="checklist-servicio">
+      <div class="service">
+      <h3 id="acerca" class="n-service">Acerca de</h3>
+      <p class="text1"><br>
+        Hola como estan todos,hola como estan todos,hola como estan todos,hola como estan todos
+        hola como estan todos,hola como estan todos,hola como estan todos,hola como estan todos
+        hola como estan todos,hola como estan todos,hola como estan todos,hola como estan todos
+        Hola como estan todos,hola como estan todos,hola como estan todos,hola como estan todos
+        hola como estan todos,hola como estan todos,hola como estan todos,hola como estan todos
+        hola como estan todos,hola como estan todos,hola como estan todos,hola como estan todos</p>
+      
+</div>
+</div>
+<img src="img/acerca.png" alt="" style="width: 300px; height:300px;">
+</div>
+
+<div class="contenedor-servicio">
+    <img src="img/contactos.png" alt="" style="width: 300px; height:300px;">
+    <div class="checklist-servicio">
+      <div class="service">
+
+      <h3 id="contactos" class="n-service">Contactos</h3>
+      <p class="text1"><br>
+        Hola como estan todos,hola como estan todos,hola como estan todos,hola como estan todos
+        hola como estan todos,hola como estan todos,hola como estan todos,hola como estan todos
+        hola como estan todos,hola como estan todos,hola como estan todos,hola como estan todos
+        Hola como estan todos,hola como estan todos,hola como estan todos,hola como estan todos
+        hola como estan todos,hola como estan todos,hola como estan todos,hola como estan todos
+        hola como estan todos,hola como estan todos,hola como estan todos,hola como estan todos</p>
+</div>
+</div>
+</div>
+</section><br><br>
+
+  <footer class="footer">
+
+  <div class="container">
+  	 	<div class="row">
+  	 		<div class="footer-col">
+  	 			<h4>Compañia</h4>
+  	 			<ul>
+
+  	 				<li><a>Esta compañia esta garantizada para ofrecerte servicios varios, 
+		       como resultados precisos y a bajo costo</a></li>
+  	 			</ul>
+  	 		</div>
+  	 		<div class="footer-col">
+  	 			<h4>Mas informacion</h4>
+  	 			<ul>
+  	 				<li><a href="#">Privacion y politica</a></li>
+  	 				<li><a href="#">Terminos y condiciones</a></li>
+  	 	
+  	 			</ul>
+  	 		</div>
+  	 		<div class="footer-col">
+  	 			<h4>Informacion contactos</h4>
+  	 			<ul>
+
+  	 				<li><a>Direccion:<br> Carrera 48 #176-10</a></li>
+  	 				<li><a>Contactos:<br> 000-000-000 <br> 111-111-111</a></li>
+  	 				<li><a>Corrreo electronico: <br> abc@gmail.com</a></li>
+  	 	
+  	 			</ul>
+  	 		</div>
+  	 		<div class="footer-col">
+  	 			<h4>Nuestras redes</h4>
+  	 			<div class="social-links">
+  	 				<a href="#"><i class="fab fa-facebook-f"></i></a>
+  	 				<a href="#"><i class="fab fa-twitter"></i></a>
+  	 				<a href="#"><i class="fab fa-instagram"></i></a>
+  	 				<a href="#"><i class="fab fa-linkedin-in"></i></a>
+  	 			</div>
+  	 		</div>
+  	 	</div>
+  	 </div><br>
+ 
+  <div class="container-footer">
+	  <div class="copyright">
+	  © 2021 Todos los derechos reservados | <a
+	  href="">Proyecto ListBalance</a>
+	  </div>
+
+  <div class="information"> 
+	  <a href=""> Privacion y politica</a>   | 
+	  <a href=""> Terminos y condiciones</a> 
+  </div>
+  </div>
+
+
+  
+  
+  </footer>
+     
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+	<script src="https://cdn.jsdelivr.net/npm/glider-js@1.7.3/glider.min.js"></script>
+	<script src="https://kit.fontawesome.com/2c36e9b7b1.js" crossorigin="anonymous"></script>
+	<script src="js/app.js"></script>
+    
+</body>
+</html>
