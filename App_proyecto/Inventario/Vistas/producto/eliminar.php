ceaf5cd24ab1c238329688f7affcbdd7dafbcfbc
JD Cousc <65689076+JDcousc@users.noreply.github.com>
2021-08-14 12:32:34 -0500

    Delete eliminar.php

--- App_proyecto/Inventario/Vistas/producto/eliminar.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $data = file_get_contents("php://input");
-    require "conexion.php";
-    $query = $pdo->prepare("DELETE FROM productos WHERE id = :id");
-    $query->bindParam(":id", $data);
-    $query->execute();
-    echo "ok";
-?>
