a79ce8a5d8f52e3eaf3b63d836d5dedf4c5cbe5f
JD Cousc <65689076+JDcousc@users.noreply.github.com>
2021-08-14 12:33:18 -0500

    Create conex.php

---
+++ App_proyecto/List Balance/conex.php
@@ -0,0 +1,18 @@
+<?php
+
+session_start();
+define('dbhost', 'localhost');
+define('dbuser', 'root');
+define('dbpass', '');
+define('dbname', 'database');
+
+ try {
+    $connect = new PDO("mysql:host=".dbhost."; dbname=".dbname, dbuser, dbpass);
+    $connect -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} 
+
+catch (PDOException $e) {
+    echo $e->getMessage();
+}
+
+?>
