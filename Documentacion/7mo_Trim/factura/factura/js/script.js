72813f81c47c587399b35c289a2ab1e065235b55
JD Cousc <65689076+JDcousc@users.noreply.github.com>
2022-02-23 13:49:47 -0500

    Delete script.js

--- Documentacion/7mo_Trim/factura/factura/js/script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $('#sidebarCollapse').on('click', function() {
-      $('#sidebar, #content').toggleClass('active');
-      $('.collapse.in').toggleClass('in');
-      $('a[aria-expanded=true]').attr('aria-expanded', 'false');
-      document.getElementById("bodyContent").style.width="100%";
-    });
-});
